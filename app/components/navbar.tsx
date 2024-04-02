6441247356ebe4e4a17fe847dc20b7fd23aa43b9
Marinus <marinus.gerdes@gmail.com>
2024-04-02 14:31:02 +0200

    Initial Commit

---
+++ app/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <>
+            <div className={'flex flex-row col-span-full'}>
+                <h2>Test</h2>
+            </div>
+        </>
+    );
+}
