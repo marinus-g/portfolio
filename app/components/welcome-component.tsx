02143f3cbd0a382f7f5794f7a3c58956da4cce50
Marinus <marinus.gerdes@gmail.com>
2024-04-03 11:39:24 +0200

    responsive sizing

--- app/components/welcome-component.tsx
+++ app/components/welcome-component.tsx
@@ -40,17 +40,18 @@ function WelcomeComponent(welcomeProps: WelcomeProps) {
 
     return <div className={'flex flex-col items-center justify-center text-center'}>
         <h1
-            className={`font-fira-code mt-2.5 select-text font-medium sm:text-4xl 2xl:text-5xl bg-gradient-to-r from-red-600 via-yellow-500-800 to-white inline-block text-transparent bg-clip-text`}>
+            className={`font-fira-code mt-2.5 select-text font-medium sm:text-4xl 2xl:text-5xl bg-gradient-to-r from-red-600 via-yellow-500-800 to-white inline-block text-transparent bg-clip-text 
+            md:text-5xl text-4xl`}>
             {welcomeText}{isTyping && <span className={'cursor text-white'}>|</span>}
         </h1>
         {showDescription && (<>
             <h3
-                className={`${welcomeProps.nerdFont.className} select-text font-light text-3xl text-white mt-3.5 fly-in`}>I am
+                className={`${welcomeProps.nerdFont.className} select-text font-light text-3xl 2xl:text-4xl text-white mt-3.5 fly-in md:text-2xl text-[18px] leading-4`}>I am
                 a
                 Software Developer interested in developing software for the general public.
             </h3>
             {showQuote &&
-            <p className={'2xl:mt-0 xl:mt-1 lg:mt-0 sm:mt-2 mt-3 fly-in'}><span
+            <p className={'2xl:mt-0 xl:mt-1 lg:mt-0 sm:mt-2 mt-3 fly-in 2xl:text-2xl'}><span
                 className={`${welcomeProps.poiretOne.className} text-white fly-in`}> &quot;Quality is more important than quantity. One home run is much better than two doubles.&quot; - Steve Jobs</span>
             </p>}
         </>)}
--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,11 @@ export default function Home() {
     return (
         <main className="flex flex-col min-h-screen w-full relative select-none">
             <div
-                className="flex flex-col bg-cover bg-center bg-no-repeat sm:h-[55vh] xl:h-[55vh] md:h-[50vh] lg:h-[50vh] h-[45vh] w-full relative"
+                className="flex flex-col
+                bg-cover bg-right-bottom bg-no-repeat
+                2xl:background-4-k
+                sm:h-[55vh] xl:h-[40vh] md:h-[50vh] lg:h-[50vh] h-[55vh] 2xl:h-[70vh] w-full relative"
                 style={{
-                    backgroundSize: "cover",
-                    backgroundPositionY: "67%",
                     backgroundRepeat: "no-repeat",
                     backgroundImage: 'url("data:image/svg+xml;base64,PHN2ZyBpZD0idmlzdWFsIiB2aWV3Qm94PSIwIDAgOTYwIDU0MCIgd2lkdGg9Ijk2MCIgaGVpZ2h0PSI1NDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHZlcnNpb249IjEuMSI+PHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9Ijk2MCIgaGVpZ2h0PSI1NDAiIGZpbGw9IiMwMDY2ZmYiPjwvcmVjdD48cGF0aCBkPSJNMCAzNTVMNDAgMzc2LjhDODAgMzk4LjcgMTYwIDQ0Mi4zIDI0MCA0MzguM0MzMjAgNDM0LjMgNDAwIDM4Mi43IDQ4MCAzNTguOEM1NjAgMzM1IDY0MCAzMzkgNzIwIDM1My4yQzgwMCAzNjcuMyA4ODAgMzkxLjcgOTIwIDQwMy44TDk2MCA0MTZMOTYwIDU0MUw5MjAgNTQxQzg4MCA1NDEgODAwIDU0MSA3MjAgNTQxQzY0MCA1NDEgNTYwIDU0MSA0ODAgNTQxQzQwMCA1NDEgMzIwIDU0MSAyNDAgNTQxQzE2MCA1NDEgODAgNTQxIDQwIDU0MUwwIDU0MVoiIGZpbGw9IiMxMTMiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJtaXRlciI+PC9wYXRoPjwvc3ZnPg==")'
                 }}>
@@ -30,7 +31,7 @@ export default function Home() {
                     <div className={'brightness-125'}>
                         <Image
                             draggable={false}
-                            className={'rounded-full fade-into-background border-4 border-blue-600 2xl:h-[200px] 2xl:w-[200px] lg:h-[150px] lg:w-[150px] md:h-[125px] md:w-[125px] sm:h-[100px] sm:w-[100px] h-[100px] w-[100px]'}
+                            className={'rounded-full fade-into-background border-4 border-blue-600 2xl:h-[100px] 2xl:w-[100px] lg:h-[150px] lg:w-[150px] md:h-[125px] md:w-[125px] sm:h-[100px] sm:w-[100px] h-[100px] w-[100px]'}
                             src={'https://avatars.githubusercontent.com/u/148557369?v=4'} alt={'User Image'}
                             placeholder="blur"
                             blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkAAIAAAoAAv/lxKUAAAAASUVORK5CYII="
